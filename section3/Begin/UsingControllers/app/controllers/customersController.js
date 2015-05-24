5030e1f31c43805ce13cfa3b8671f04baab9aca4
Randy Davis <randy_davis@outlook.com>
2015-05-23 20:33:31 -0500

    add creating_module.html and add app.js to show basic setup of a module

--- section3/Begin/UsingControllers/app/controllers/customersController.js
+++ section3/Begin/UsingControllers/app/controllers/customersController.js
@@ -1,29 +1,29 @@
 // current syntax in view file is --- ng-controller="CustomersController"
-// function CustomersController($scope) {
-//     $scope.sortBy = 'name';
-//     $scope.reverse = false;
+function CustomersController($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
     
-//     $scope.customers = [{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99}, {joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}];
-//     $scope.doSort = function(propName) {
-//         $scope.sortBy= propName;
-//         $scope.reverse=!$scope.reverse;
-//     };
-// }
+    $scope.customers = [{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99}, {joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}];
+    $scope.doSort = function(propName) {
+        $scope.sortBy= propName;
+        $scope.reverse=!$scope.reverse;
+    };
+}
 
 // Alternative syntax to use "controller as" in view file
-function CustomersController($scope) {
-  $scope.sortBy = 'name';
-  $scope.reverse = false;
+// function CustomersController($scope) {
+//   $scope.sortBy = 'name';
+//   $scope.reverse = false;
 
-  this.customers = [
-    {joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956},
-    {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},
-    {joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99},
-    {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}
-  ];
+//   this.customers = [
+//     {joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956},
+//     {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},
+//     {joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99},
+//     {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}
+//   ];
 
-  $scope.doSort = function(propName) {
-      $scope.sortBy= propName;
-      $scope.reverse=!$scope.reverse;
-  };
-}
+//   $scope.doSort = function(propName) {
+//       $scope.sortBy= propName;
+//       $scope.reverse=!$scope.reverse;
+//   };
+// }
