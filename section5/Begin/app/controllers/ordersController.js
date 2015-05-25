be055c41681ba0219d8995ed0eb99a16b098fd14
Randy Davis <randy_davis@outlook.com>
2015-05-24 23:45:44 -0500

    create section 5 directory for Factories and Services; refactor code from last section to use customersFactory but still need to inject factory into controller

---
+++ section5/Begin/app/controllers/ordersController.js
@@ -0,0 +1,25 @@
+(function() {
+    
+    var OrdersController = function ($scope, $routeParams) {
+        var customerId = $routeParams.customerId;
+        $scope.orders = null;
+        
+        function init() {
+            //Search the customers for the customerId
+            for (var i=0,len=$scope.customers.length;i<len;i++) {
+               if ($scope.customers[i].id === parseInt(customerId)) {
+                   $scope.orders = $scope.customers[i].orders;
+                   break;
+               }
+            }
+        }
+        
+        init();
+    };
+    
+    OrdersController.$inject = ['$scope', '$routeParams'];
+
+    angular.module('customersApp')
+      .controller('OrdersController', OrdersController);
+    
+}());
